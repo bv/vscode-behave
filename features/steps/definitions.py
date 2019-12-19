87222effb93b29ff23c7a5f6f252cd58e3f36c29
Volodymyr BOBER <volodymyr.bober@geo-satis.com>
2019-12-19 20:51:04 +0100

    Behave skeleton with extension settings.

---
+++ features/steps/definitions.py
@@ -0,0 +1,33 @@
+from behave import *
+
+@given(u'these things')
+def step_given(context):
+    pass
+
+@when(u'I do action')
+def step_when(context):
+    pass
+
+@then(u'response should appear')
+def step_then(context):
+    pass
+
+@step(u'make a step')
+def step_make(context):
+    pass
+
+@given('other things too')
+def step_given_too(context):
+    pass
+
+@when('I do another action')
+def step_when_too(context):
+    pass
+
+@then('another one should appear too')
+def step_then_too(context):
+    pass
+
+@step('make another step')
+def step_make_too(context):
+    pass
